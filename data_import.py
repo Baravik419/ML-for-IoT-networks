4107e5126de710292e32c7b127f1367c68c21d93
Baravik419 <ervin.borkovski@gmail.com>
2026-04-07 00:09:28 +0300

    Data import, pre-manipulation and creation of a joint data frame

---
+++ data_import.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+# Importing the dataset from csv to pandas data frame
+
+Fridge = pd.read_csv("ToN_IoT dataset (IoT only)/Processed_IoT_dataset/IoT_Fridge.csv")
+Garage_Door = pd.read_csv("ToN_IoT dataset (IoT only)/Processed_IoT_dataset/IoT_Garage_Door.csv", dtype={"sphone_signal": "str"})
+Gps_Tracker = pd.read_csv("ToN_IoT dataset (IoT only)/Processed_IoT_dataset/IoT_GPS_Tracker.csv")
+Modbus = pd.read_csv("ToN_IoT dataset (IoT only)/Processed_IoT_dataset/IoT_Modbus.csv")
+Motion_Light = pd.read_csv("ToN_IoT dataset (IoT only)/Processed_IoT_dataset/IoT_Motion_Light.csv")
+Thermostat = pd.read_csv("ToN_IoT dataset (IoT only)/Processed_IoT_dataset/IoT_Thermostat.csv")
+Weather = pd.read_csv("ToN_IoT dataset (IoT only)/Processed_IoT_dataset/IoT_Weather.csv")
+
+# Creating a new column to specify the device type for the joint data frame
+
+Fridge["device_type"] = "fridge"
+Garage_Door["device_type"] = "garage_door"
+Gps_Tracker["device_type"] = "gps_tracker"
+Modbus["device_type"] = "modbus"
+Motion_Light["device_type"] = "motion_light"
+Thermostat["device_type"] = "thermostat"
+Weather["device_type"] = "weather"
+
+# Joining all the data frames
+
+IoT_data = pd.concat([Fridge, Garage_Door, Gps_Tracker, Modbus, Motion_Light, Thermostat, Weather], ignore_index=True)
+
+#print(IoT_data.shape)
+#print(IoT_data.columns)
+#print(IoT_data.head())
+
+# Clearing the data frame for unuseful data
+
+IoT_data = IoT_data.drop(columns=["date", "time", "label"])
+
+#print(IoT_data.shape)
+#print(IoT_data.columns)
