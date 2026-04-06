8af2b94153194fef017d0acf508825798c94af4e
Baravik419 <ervin.borkovski@gmail.com>
2026-04-07 01:41:18 +0300

    Started with data manipulation – conversion to numerical, NaN to 0, encoding and splitting

--- data_import.py
+++ data_import.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 # Importing the dataset from csv to pandas data frame
-
 Fridge = pd.read_csv("ToN_IoT dataset (IoT only)/Processed_IoT_dataset/IoT_Fridge.csv")
 Garage_Door = pd.read_csv("ToN_IoT dataset (IoT only)/Processed_IoT_dataset/IoT_Garage_Door.csv", dtype={"sphone_signal": "str"})
 Gps_Tracker = pd.read_csv("ToN_IoT dataset (IoT only)/Processed_IoT_dataset/IoT_GPS_Tracker.csv")
@@ -11,7 +10,6 @@ Thermostat = pd.read_csv("ToN_IoT dataset (IoT only)/Processed_IoT_dataset/IoT_T
 Weather = pd.read_csv("ToN_IoT dataset (IoT only)/Processed_IoT_dataset/IoT_Weather.csv")
 
 # Creating a new column to specify the device type for the joint data frame
-
 Fridge["device_type"] = "fridge"
 Garage_Door["device_type"] = "garage_door"
 Gps_Tracker["device_type"] = "gps_tracker"
@@ -21,7 +19,6 @@ Thermostat["device_type"] = "thermostat"
 Weather["device_type"] = "weather"
 
 # Joining all the data frames
-
 IoT_data = pd.concat([Fridge, Garage_Door, Gps_Tracker, Modbus, Motion_Light, Thermostat, Weather], ignore_index=True)
 
 #print(IoT_data.shape)
@@ -29,8 +26,8 @@ IoT_data = pd.concat([Fridge, Garage_Door, Gps_Tracker, Modbus, Motion_Light, Th
 #print(IoT_data.head())
 
 # Clearing the data frame for unuseful data
-
 IoT_data = IoT_data.drop(columns=["date", "time", "label"])
 
+IoT_data.to_csv("IoT_data.csv", index=False)
 #print(IoT_data.shape)
 #print(IoT_data.columns)
