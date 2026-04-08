6f1f14cc3fe8110b9c6c9f14f4bf247ac0322b0e
Baravik419 <ervin.borkovski@gmail.com>
2026-04-08 22:39:10 +0300

    Fixed major issue with the model, as it was not using all folds correctly, added metrics, separed code into functions

--- data_manipulation.py
+++ data_manipulation.py
@@ -5,77 +5,92 @@ from data_import import IoT_data
 from imblearn.over_sampling import SMOTE
 from collections import Counter
 
-# Separating features and targets
-X = IoT_data.drop(columns=["type"])
-y = IoT_data["type"]
+def prepare_data():
 
-#print(X.shape)
-#print(y.shape)
-#print(X.columns)
-#print(y.head())
-#print(X.dtypes)
+    # Separating features and targets
+    X = IoT_data.drop(columns=["type"])
+    y = IoT_data["type"]
 
-# Conversion of the data from strings to numerical
+    #print(X.shape)
+    #print(y.shape)
+    #print(X.columns)
+    #print(y.head())
+    #print(X.dtypes)
 
-Categorical_Columns = ["temp_condition", "device_type", "door_state", "sphone_signal", "light_status"]
-for column in Categorical_Columns:
-    X[column] = X[column].astype(str).str.strip()
+    # Conversion of the data from strings to numerical
 
-    # Cleaning the sphone_signal feature as it is messy
+    Categorical_Columns = ["temp_condition", "device_type", "door_state", "sphone_signal", "light_status"]
+    for column in Categorical_Columns:
+        X[column] = X[column].astype(str).str.strip()
 
-X["sphone_signal"] = X["sphone_signal"].astype(str).str.strip().str.lower()
-X["sphone_signal"] = X["sphone_signal"].replace({
-    "0": "false",
-    "1": "true"
-})
+        # Cleaning the sphone_signal feature as it is messy
 
-X = pd.get_dummies(X, columns=Categorical_Columns)
+    X["sphone_signal"] = X["sphone_signal"].astype(str).str.strip().str.lower()
+    X["sphone_signal"] = X["sphone_signal"].replace({
+        "0": "false",
+        "1": "true"
+    })
 
-bool_columns = X.select_dtypes(include=["bool"]).columns
-X[bool_columns] = X[bool_columns].astype(int)
+    X = pd.get_dummies(X, columns=Categorical_Columns)
 
-    # NaN to 0
+    bool_columns = X.select_dtypes(include=["bool"]).columns
+    X[bool_columns] = X[bool_columns].astype(int)
 
-X = X.fillna(0)
+        # NaN to 0
 
-#print(X.isnull().sum().sum())
+    X = X.fillna(0)
 
-#print(X.shape)
-#print(X.columns)
-#print(X.head())
-#print(X.dtypes)
+    #print(X.isnull().sum().sum())
 
-#X.to_csv("X.csv", index=False)
+    #print(X.shape)
+    #print(X.columns)
+    #print(X.head())
+    #print(X.dtypes)
 
-Label_Encoder = LabelEncoder()
-y = Label_Encoder.fit_transform(y)
+    #X.to_csv("X.csv", index=False)
 
-#print(y[:10])
-#print(Label_Encoder.classes_)
+    Label_Encoder = LabelEncoder()
+    y = Label_Encoder.fit_transform(y)
 
-# Splitting the data into train and test sets
+    #print(y[:10])
+    #print(Label_Encoder.classes_)
 
-skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+    return X, y, Label_Encoder
 
-for train_index, test_index in skf.split(X, y):
-    X_train = X.iloc[train_index]
-    X_test = X.iloc[test_index]
-    y_train = y[train_index]
-    y_test = y[test_index]
+def generate_folds(use_SMOTE=True):
+    X, y, Label_Encoder = prepare_data()
 
-    # Data scaling
+    # Splitting the data into train and test sets
 
-    scaler = StandardScaler()
-    X_train_scaled = scaler.fit_transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
-    # Data balancing
+    folds = []
 
-    smote = SMOTE(random_state=42)
-    X_train_balanced, y_train_balanced = smote.fit_resample(X_train_scaled, y_train)
+    for fold_number, (train_index, test_index) in enumerate(skf.split(X, y), start=1):
+        X_train = X.iloc[train_index]
+        X_test = X.iloc[test_index]
+        y_train = y[train_index]
+        y_test = y[test_index]
 
-print("X_train_scaled: ",X_train_scaled.shape)
-print("X_test_scaled: ",X_test_scaled.shape)
+        # Data scaling
 
-print("X_train_balanced: ",X_train_balanced.shape)
-print("y_train_balanced: ",len(y_train_balanced))
+        scaler = StandardScaler()
+        X_train_scaled = scaler.fit_transform(X_train)
+        X_test_scaled = scaler.transform(X_test)
+
+        # Data balancing
+        if use_SMOTE:
+            smote = SMOTE(random_state=42)
+            X_train_final, y_train_final = smote.fit_resample(X_train_scaled, y_train)
+        else:
+            X_train_final, y_train_final = X_train_scaled, y_train
+
+        folds.append({
+            "fold_number: ": fold_number,
+            "X_train: ": X_train_final,
+            "y_train: ": y_train_final,
+            "X_test: ": X_test_scaled,
+            "y_test: ": y_test,
+            "scaler: ": scaler
+        })
+    return folds, Label_Encoder, X.columns.tolist()
